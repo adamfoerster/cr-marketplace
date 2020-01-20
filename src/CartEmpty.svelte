bae64571d70088694422b60ad05eb5f4d0eeef98
Adam Foerster <adam.foerster@protonmail.com>
2020-01-19 23:55:03 -0200

    [feat] empty cart comp

---
+++ src/CartEmpty.svelte
@@ -0,0 +1,16 @@
+<style>
+  .empty-cart {
+    display: flex;
+    flex-direction: column;
+    margin: 3rem 1rem;
+    color: var(--medium-gray);
+  }
+  .empty-cart img {
+    margin: 0 0 1rem;
+  }
+</style>
+
+<div class="empty-cart">
+  <img src="/assets/cart-icon.svg" alt="Carrinho vazio" />
+  Até o momento o seu carrinho está vazio.
+</div>
