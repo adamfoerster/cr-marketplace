c8499f1184cfc50605a312defe5ff400b7351020
Adam Foerster <adam.foerster@protonmail.com>
2020-01-19 23:54:26 -0200

    [feat] helper funcs for sort and currency display

---
+++ src/helpers.js
@@ -0,0 +1,33 @@
+export const by = {
+  score: (a, b) => {
+    if (b.score > a.score) {
+      return 1;
+    }
+    if (a.score > b.score) {
+      return -1;
+    }
+    return 0;
+  },
+  price: (a, b) => {
+    if (b.price > a.price) {
+      return -1;
+    }
+    if (a.price > b.price) {
+      return 1;
+    }
+    return 0;
+  },
+  alpha: (a, b) => {
+    if (b.name > a.name) {
+      return -1;
+    }
+    if (a.name > b.name) {
+      return 1;
+    }
+    return 0;
+  }
+};
+
+export const brl = amount => {
+  return amount.toFixed(2).split('.').join(',');
+};
