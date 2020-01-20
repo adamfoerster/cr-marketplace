929887c6637d0869bbcb10ae67d0f716d3a0fa6e
Adam Foerster <adam.foerster@protonmail.com>
2020-01-19 23:49:06 -0200

    [feat] global state store created

--- src/store.js
+++ src/store.js
@@ -1,15 +1,41 @@
-import { writable } from 'svelte/store';
+import { writable, get } from 'svelte/store';
 
-export const products = writable([]);
-export const order = writable('price');
-export const cart = writable([]);
+let globalStore = null;
 
-export const fetchProducts = () => {
-  fetch('products.json')
-    .then(response => response.json())
-    .then(json => products.set(json));
+export const getStore = () => {
+  if (!globalStore) {
+    globalStore = createStore();
+  }
+  return globalStore;
 };
 
-products.subscribe(value => {
-  console.log(value);
-});
+const createStore = () => {
+  const products = writable([]);
+  const sorting = writable('score');
+  const cart = writable([]);
+
+  return {
+    products$: products.subscribe,
+    sorting$: sorting.subscribe,
+    cart$: cart.subscribe,
+
+    fetchProducts: () => {
+      fetch('products.json')
+        .then(response => response.json())
+        .then(json => products.set(json));
+    },
+
+    addToCart: newProduct => {
+      if (get(cart).find(item => item.id === newProduct.id)) {
+        return;
+      }
+      cart.update(p => [...p, newProduct]);
+    },
+
+    removeFromCart: id => {
+      cart.set(get(cart).filter(item => item.id !== id));
+    },
+
+    setSorting: sortingMethod => sorting.set(sortingMethod)
+  };
+};
