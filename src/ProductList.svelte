17cb196e055dcd413dcf410f2cf99a6dfd9048db
Adam Foerster <adam.foerster@protonmail.com>
2020-01-19 23:56:40 -0200

    [feat] product list

--- src/ProductList.svelte
+++ src/ProductList.svelte
@@ -1,23 +1,38 @@
 <script>
+  import { onMount } from "svelte";
   import ProductItem from "./ProductItem.svelte";
-  import { products, order } from "./store.js";
+  import { getStore } from "./store.js";
+  import { by } from "./helpers.js";
 
   let orderedProducts = [];
+  let sorting = "price";
 
-  products.subscribe(p => orderedProducts = p);
+  onMount(() => {
+    const store = getStore();
+    store.sorting$(o => {
+      sorting = o;
+      orderedProducts = orderedProducts.sort(by[sorting]);
+    });
+    store.products$(p => (orderedProducts = p.sort(by[sorting])));
+  });
 </script>
 
 <style>
   .container {
     display: grid;
-    grid-template-columns: auto auto auto;
+    grid-template-columns: calc(33.33% - 1rem) calc(33.33% - 1rem) calc(
+        33.33% - 1rem
+      );
     grid-template-rows: auto;
     grid-gap: 1.5rem;
+  }
+  .container:hover {
+    cursor: pointer;
   }
 </style>
 
 <div class="container">
   {#each orderedProducts as product}
-  <ProductItem {...product}/>
+    <ProductItem {...product} />
   {/each}
 </div>
