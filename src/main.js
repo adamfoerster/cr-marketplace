d49b759a2bcd649bc76f5322e73815ec6c99920c
Adam Foerster <adam.foerster@protonmail.com>
2020-01-26 17:34:29 -0200

    fix env

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import App from './App.svelte';
 
 const app = new App({
-	target: document.body,
-	props: {
-		environment: {
-			api: 'http://localhost:5000/'
-		},
-	}
+  target: document.body,
+  props: {
+    environment: {
+      api:
+        location.hostname === 'hostname'
+          ? 'http://localhost:5000/'
+          : 'https://cr-marketplace.netlify.com'
+    }
+  }
 });
 
 export default app;
